099fdc0b430b655452ee27f4dd7a975ee6e6a35c
olecsandr009 <sasha2005vit@gmail.com>
2024-05-09 16:40:09 +0300

    update: text_edit window

--- components/ui/inputs/text_edit/window/window.py
+++ components/ui/inputs/text_edit/window/window.py
@@ -9,12 +9,12 @@ from components.ui.buttons.button import ButtonStyle
 
 class Window(QWidget):
     # Initialize the apply widget
-    def __init__(self, data, key:str, parent: Optional[QWidget] = None) -> None:
+    def __init__(self, data, key: str, parent: Optional[QWidget] = None) -> None:
         super(Window, self).__init__(parent)
 
         # Set object name
         self.setObjectName("inputs_window")
-        
+
         self.data = data
         self.key = key
 
@@ -26,7 +26,7 @@ class Window(QWidget):
         # Default widow size
         self.width = 300
         self.height = 400
-        
+
         # Default margin values
         self.margin = [8, 8, 8, 8]
         # Default alignment value
@@ -39,12 +39,10 @@ class Window(QWidget):
         self.__configureButtonLayout()
         self.__configureButtonClose()
         self.__configureButtonApply()
-        
-        
+
         self.buttons_bottom.setLayout(self.buttons_layout)
         self.window_layout.addWidget(self.buttons_bottom)
-        
-        
+
         self.setLayout(self.window_layout)
 
     # Set the margin values
@@ -80,38 +78,37 @@ class Window(QWidget):
         self.window_layout = QVBoxLayout(self)
         self.window_layout.setContentsMargins(self.margin[0], self.margin[1], self.margin[2], self.margin[3])
         self.window_layout.setAlignment(self.alignment)
-        
+
     # Configure the window text edit
     def __configureTextEdit(self):
         self.text_edit = QTextEdit(self)
         self.text_edit.setObjectName("inputs_window_text_edit")
-        
+
         self.window_layout.addWidget(self.text_edit)
-    
+
     # Configure the button layout
     def __configureButtonLayout(self):
         self.buttons_bottom = QWidget(self)
         self.buttons_bottom.setObjectName("inputs_window_buttons")
-        
+
         self.buttons_layout = QHBoxLayout(self.buttons_bottom)
         self.buttons_layout.setContentsMargins(0, 30, 0, 0)
         self.buttons_layout.setAlignment(self.alignment)
-        
+
     # Configure the button close
     def __configureButtonClose(self):
         self.button_close = Button(self.buttons_bottom)
         self.button_close.setButtonLayout("Відмінити", ButtonStyle.BORDER)
-        
+
         self.button_close.clicked(self.buttonCloseClickHandler)
-        
+
         self.buttons_layout.addWidget(self.button_close)
-        
+
     # Configure the button apply
     def __configureButtonApply(self):
         self.button_apply = Button(self.buttons_bottom)
         self.button_apply.setButtonLayout("Зберегти", ButtonStyle.FILL)
-        
+
         self.button_apply.clicked(self.buttonApplyClickHandler)
-        
+
         self.buttons_layout.addWidget(self.button_apply)
-        
