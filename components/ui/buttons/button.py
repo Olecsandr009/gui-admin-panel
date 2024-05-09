7c2e89381cac21a7402e7236e3b54ccf9a6c0cd3
olecsandr009 <sasha2005vit@gmail.com>
2024-05-09 14:43:13 +0300

    feat: create UI button_default.py

--- components/ui/buttons/button.py
+++ components/ui/buttons/button.py
@@ -8,53 +8,61 @@ from typing import Optional
 
 from components.ui.buttons.button_border import ButtonBorder
 from components.ui.buttons.button_fill import ButtonFill
+from components.ui.buttons.button_default import ButtonDefault
+
 
 class ButtonStyle(Enum):
     DEFAULT = 1
     BORDER = 2
     FILL = 3
 
+
 class Button(QFrame):
     def __init__(self, parent: Optional[QWidget] = None) -> None:
         super(Button, self).__init__(parent)
-        
+
         # Set the object name
         self.setObjectName("button")
-        
+
         # Default margin values
         self.margin = [0, 0, 0, 0]
         # Default alignment value
         self.alignment = Qt.AlignmentFlag.AlignLeading
-        
+        # Default button var
+        self.button = None
+
         self.__setupLayout()
-        
+
         self.setLayout(self.button_layout)
-        
+
     # Set the margin values
     def setContentMargins(self, left: int, top: int, right: int, bottom: int):
         self.button_layout.setContentsMargins(left, top, right, bottom)
-        
+
     # Set the button layout
     def setButtonLayout(self, text: str, style: ButtonStyle):
         self.button = None
-        
+
         if style == ButtonStyle.BORDER:
             self.button = ButtonBorder(text=text, parent=self)
         elif style == ButtonStyle.FILL:
             self.button = ButtonFill(text=text, parent=self)
-            
-        if self.button: self.button.setCursor(QCursor(Qt.CursorShape.PointingHandCursor))
-        
+        elif style == ButtonStyle.DEFAULT:
+            self.button = ButtonDefault(text=text, parent=self)
+
+        if self.button:
+            self.button.setCursor(QCursor(Qt.CursorShape.PointingHandCursor))
+
         self.button_layout.addWidget(self.button)
-            
+
     # Set the alignment value
     def setAlignment(self, alignment: Qt.AlignmentFlag = Qt.AlignmentFlag.AlignLeading):
         self.button_layout.setAlignment(alignment)
-            
+
     # Set the text font
     def setFont(self, font: QFont) -> None:
         self.button.setFont(font)
-            
+
     # Paint widget
     def paintEvent(self, a0: QPaintEvent | None) -> None:
         o = QStyleOption()
@@ -62,10 +70,9 @@ class Button(QFrame):
         p = QPainter(self)
         self.style().drawPrimitive(QStyle.PrimitiveElement.PE_Widget, o, p, self)
         return super().paintEvent(a0)
-    
+
     # Setup the layout
     def __setupLayout(self):
         self.button_layout = QVBoxLayout(self)
         self.button_layout.setContentsMargins(self.margin[0], self.margin[1], self.margin[2], self.margin[3])
         self.button_layout.setAlignment(self.alignment)
-        
