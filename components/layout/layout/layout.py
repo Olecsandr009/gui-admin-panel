f36eb565d117b1c37fcef7e5e0b1b6b6407d727c
olecsandr009 <sasha2005vit@gmail.com>
2024-05-11 00:26:22 +0300

    update: add new abstract methods

--- components/layout/layout/layout.py
+++ components/layout/layout/layout.py
@@ -4,22 +4,36 @@ from PyQt6.QtCore import Qt
 from abc import ABC, abstractmethod
 
 
-class LayoutInterface(ABC):
+class LayoutInterface:
     @abstractmethod
-    def setLayoutMargins(self, left: int, top: int, right: int, bottom: int):
+    def setLayoutMargins(self, left: int, top: int, right: int, bottom: int) -> None:
         pass
 
     @abstractmethod
-    def setLayoutAlignment(self, alignment: Qt.AlignmentFlag):
+    def setLayoutAlignment(self, alignment: Qt.AlignmentFlag) -> None:
+        pass
+
+    @abstractmethod
+    def addLayoutWidget(self, widget: QWidget) -> None:
+        pass
+
+    @abstractmethod
+    def addLayoutSpacing(self, space: int) -> None:
         pass
 
 
-class Layout(QBoxLayout, LayoutInterface):
+class Layout(QBoxLayout):
     def __init__(self, direction: QBoxLayout.Direction, parent: QWidget = None):
         super().__init__(direction, parent)
 
-    def setLayoutMargins(self, left: int, top: int, right: int, bottom: int):
-        self.setContentsMargins(left, top, right, bottom)
-
-    def setLayoutAlignment(self, alignment: Qt.AlignmentFlag):
-        self.setAlignment(alignment)
+    # def setLayoutMargins(self, left: int, top: int, right: int, bottom: int):
+    #     self.setContentsMargins(left, top, right, bottom)
+    #
+    # def setLayoutAlignment(self, alignment: Qt.AlignmentFlag):
+    #     self.setAlignment(alignment)
+    #
+    # def addLayoutSpacing(self, space: int) -> None:
+    #     self.addSpacing(space)
+    #
+    # def addLayoutWidget(self, widget: QWidget) -> None:
+    #     self.addWidget(widget)
