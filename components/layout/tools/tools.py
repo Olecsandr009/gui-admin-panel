9f31392e3a910fc2a3288d69d0d077045eb38c63
olecsandr009 <sasha2005vit@gmail.com>
2024-05-11 05:02:17 +0300

    update: tool button settings

--- components/layout/tools/tools.py
+++ components/layout/tools/tools.py
@@ -1,5 +1,6 @@
 from PyQt6.QtWidgets import QWidget, QStyleOption, QStyle
 from PyQt6.QtGui import QPaintEvent, QPainter
+from PyQt6.QtCore import Qt
 
 from components.layout.container.container import Container
 from components.layout.tools.tools_buttons.tools_buttons import ToolsButtons
@@ -10,9 +11,23 @@ class Tools(Container):
     def __init__(self, main: QWidget):
         super().__init__()
 
+        self.main = main
+
+        self.__configureLayout()
+        self.__configureButtons()
+
+    # Configure the layout
+    def __configureLayout(self):
         self.addLayout(self.Direction.LeftToRight)
 
-        self.tools_button = ToolsButtons(main)
+        self.layout.setContentsMargins(0, 0, 0, 0)
+
+    # Configure the tools buttons
+    def __configureButtons(self):
+        self.tools_button = ToolsButtons(self.main)
+        self.tools_button.layout.setAlignment(Qt.AlignmentFlag.AlignRight)
+        self.tools_button.layout.setContentsMargins(0, 0, 0, 0)
+        self.tools_button.setFixedHeight(30)
 
         self.layout.addWidget(self.tools_button)
 
