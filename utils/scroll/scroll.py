84404aa887765e1dc3a9cf48b23ad9cbb153a43d
olecsandr009 <sasha2005vit@gmail.com>
2024-05-13 23:52:28 +0300

    update: update custom scroll

--- utils/scroll/scroll.py
+++ utils/scroll/scroll.py
@@ -1,6 +1,8 @@
-from PyQt6.QtWidgets import QScrollArea, QWidget, QStyleOption, QStyle, QVBoxLayout
+from PyQt6.QtWidgets import QScrollArea, QWidget, QStyleOption, QStyle
 from PyQt6.QtGui import QPainter, QPaintEvent
 from PyQt6.QtCore import Qt
+
+from components.layout import Frame
 
 
 class Scroll(QScrollArea):
@@ -15,14 +17,15 @@ class Scroll(QScrollArea):
         self.scroll_bar_policy = Qt.ScrollBarPolicy.ScrollBarAlwaysOff
         
         self.__setupScroll()
+        self.__configureWidget()
         
     # Set the horizontal scroll bar policy
     def setHorizontalScrollBarPolicy(self, a0: Qt.ScrollBarPolicy) -> None:
         return super().setHorizontalScrollBarPolicy(a0)
         
-    # Set the scroll widget
-    def setWidget(self, w: QWidget | None) -> None:
-        return super().setWidget(w)
+    # Add the scroll widget
+    def addWidget(self, widget: QWidget) -> None:
+        self.scroll_layout.layout.addWidget(widget)
     
     # Paint widget
     def paintEvent(self, a0: QPaintEvent | None) -> None:
@@ -36,3 +39,11 @@ class Scroll(QScrollArea):
     def __setupScroll(self):
         self.setHorizontalScrollBarPolicy(self.scroll_bar_policy)
         self.setWidgetResizable(True)
+
+    # Configure the scroll widget
+    def __configureWidget(self):
+        self.scroll_layout = Frame(self)
+
+        self.scroll_layout.addLayout(Frame.Direction.TopToBottom)
+
+        self.setWidget(self.scroll_layout)
